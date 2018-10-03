10884dbc47a559f247420b268e766c3a2fc08a8a
touyou <fujiyou1121@gmail.com>
2018-10-03 14:35:13 +0900

    WIP #1 Swift Convert (add bold style)

--- iosMath/render/internal/MTTypeSetter.swift
+++ iosMath/render/internal/MTTypeSetter.swift
@@ -68,17 +68,17 @@ func getInterElementSpaceArrayIndex(for type: MTMathAtomType, row: Bool) -> UInt
 
 // MARK: - Font styles
 
-// Alphabet, Number or Greek Character
+// MARK: Alphabet, Number or Greek Character
 let unicodeGreekLowerStart: UniChar = 0x03B1
 let unicodeGreekLowerEnd: UniChar = 0x03C9
 let unicodeGreekCapitalStart: UniChar = 0x0391
 let unicodeGreekCapitalEnd: UniChar = 0x03A9
 
-let isLowerEn = { (ch: UniChar) -> Bool in return ch >= "a" && ch <= "z" }
-let isUpperEn = { (ch: UniChar) -> Bool in return ch >= "A" && ch <= "Z" }
-let isNumber = { (ch: UniChar) -> Bool in return ch >= "0" && ch <= "9" }
-let isLowerGreek = { (ch: UniChar) -> Bool in return ch >= unicodeGreekLowerStart && ch <= unicodeGreekLowerEnd }
-let isCapitalGreek = { (ch: UniChar) -> Bool in return ch >= unicodeGreekCapitalStart && ch <= unicodeGreekCapitalEnd }
+func isLowerEn(_ ch: UniChar) -> Bool { return ch >= "a" && ch <= "z" }
+func isUpperEn(_ ch: UniChar) -> Bool { return ch >= "A" && ch <= "Z" }
+func isNumber(_ ch: UniChar) -> Bool { return ch >= "0" && ch <= "9" }
+func isLowerGreek(_ ch: UniChar) -> Bool { return ch >= unicodeGreekLowerStart && ch <= unicodeGreekLowerEnd }
+func isCapitalGreek(_ ch: UniChar) -> Bool { return ch >= unicodeGreekCapitalStart && ch <= unicodeGreekCapitalEnd }
 
 func greekSymbolOrder(_ ch: UniChar) -> Int? {
     let greekSymbols: [UniChar] = [0x03F5, 0x03D1, 0x03F0, 0x03D5, 0x03F1, 0x03D6]
@@ -86,9 +86,9 @@ func greekSymbolOrder(_ ch: UniChar) -> Int? {
     return greekSymbols.firstIndex(of: ch)
 }
 
-let isGreekSymbol = { (ch: UniChar) -> Bool in greekSymbolOrder(ch) != nil }
+func isGreekSymbol(_ ch: UniChar) -> Bool { greekSymbolOrder(ch) != nil }
 
-// Italic style
+// MARK: Italic style
 let unicodePlanksConstant: UniChar = 0x210e
 let unicodeMathCapitalItalicStart: UTF32Char = 0x1D434
 let unicodeMathLowerItalicStart: UTF32Char = 0x1D44E
@@ -119,8 +119,32 @@ func getItalicized(_ ch: UniChar) -> UTF32Char {
     return unicode
 }
 
-// Bold style
-
+// MARK: Bold style
 let unicodeMathCapitalBoldStart: UTF32Char = 0x1D400
 let unicodeMathLowerBoldStart: UTF32Char = 0x1D41A
+let unicodeGreekCapitalBoldStart: UTF32Char = 0x1D6A8
+let unicodeGreekLowerBoldStart: UTF32Char = 0x1D6C2
+let unicodeGreekSymbolBoldStart: UTF32Char = 0x1D6DC
+let unicodeNumberBoldStart: UTF32Char = 0x1D7CE
+
+func getBold(_ ch: UniChar) -> UTF32Char {
+
+    var unicode: UTF32Char = ch
+    if isUpperEn(ch) {
+        unicode = unicodeMathCapitalBoldStart + (ch - "A")
+    } else if isLowerEn(ch) {
+        unicode = unicodeMathLowerBoldStart + (ch - "a")
+    } else if isCapitalGreek(ch) {
+        unicode = unicodeGreekCapitalBoldStart + (ch - unicodeGreekCapitalStart)
+    } else if isLowerGreek(ch) {
+        unicode = unicodeGreekLowerBoldStart + (ch - unicodeGreekLowerStart)
+    } else if isGreekSymbol(ch) {
+        return unicodeGreekSymbolBoldStart + greekSymbolOrder(ch)!
+    } else if isNumber(ch) {
+        unicode = unicodeNumberBoldStart + (ch - "0")
+    }
+    return unicode
+}
+
+// MARK: bold italic
 
